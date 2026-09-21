bca2762270f9f277245f26f38044e5fa3899d232
eyad473 <76595914+eyad473@users.noreply.github.com>
2026-09-21 20:00:47 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,45 @@
 const CACHE_NAME = 'abu-oreiban-v14';
 const ASSETS = ['./','./index.html','./manifest.json'];
-self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting())); });
-self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(c => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(k => k !== CACHE_NAME)
+            .map(k => caches.delete(k))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', event => {
-  if(event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response=>{
-    const copy=response.clone(); caches.open(CACHE_NAME).then(c=>c.put(event.request,copy)); return response;
-  }).catch(()=>caches.match('./index.html'))));
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(cached =>
+        cached ||
+        fetch(event.request)
+          .then(response => {
+            const copy = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(c => c.put(event.request, copy));
+
+            return response;
+          })
+          .catch(() => caches.match('./index.html'))
+      )
+  );
 });
