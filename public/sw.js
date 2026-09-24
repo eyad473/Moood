e0472ac6398fe6056fdd1ba2a2ea3d089a039cd0
eyad473 <76595914+eyad473@users.noreply.github.com>
2026-09-24 21:22:42 +0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "abu-areiban-v51-5-offline-1";
+const CACHE = "abu-areiban-v52-5-offline-1";
 const APP_SHELL = ["./", "./index.html", "./app.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
